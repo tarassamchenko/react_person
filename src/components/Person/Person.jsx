27397802eb4aa03c488abf5fa5009be0ed68be52
Taras <tarasamchenko@gmail.com>
2024-04-18 00:15:22 +0300

    add getMariageStatus function and small improves

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,22 +1,21 @@
 export const Person = ({ person }) => {
-  let MariageStatus = 'I am not married';
-  let partner = 'husband';
+  const { sex } = person;
 
-  if (person.isMarried) {
-    if (person.sex === 'm') {
-      partner = 'wife';
+  function getMarriageStatus(human) {
+    if (!human.isMarried) {
+      return 'I am not married';
     }
 
-    MariageStatus = `${person.partnerName} is my ${partner}`;
+    const partner = sex === 'm' ? 'wife' : 'husband';
+
+    return `${human.partnerName} is my ${partner}`;
   }
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age !== undefined && (
-        <p className="Person__age">I am {person.age}</p>
-      )}
-      <p className="Person__partner">{MariageStatus}</p>
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+      <p className="Person__partner">{getMarriageStatus(person)}</p>
     </section>
   );
 };
